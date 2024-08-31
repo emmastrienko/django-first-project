c1dc11ad13a727dd6aa0634433210b5a31abe102
Emma Strienko <mstrienko@gmail.com>
2024-08-31 12:51:32 +0300

    added db

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'mysite_usr',
+        'PASSWORD': 'mysite_pass',
     }
 }
 
