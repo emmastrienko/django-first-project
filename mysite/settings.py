eaf92f5360d91881592e8e4431bee4c248e5d260
Emma Strienko <mstrienko@gmail.com>
2024-10-11 13:19:57 +0300

    access

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4s6=e!d)z@)%blh%-o9bn0n4xe$+-0!!f+jr#-1s)7hq)le(=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
